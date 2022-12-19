5ed6c9f925dea61c90772d7fdae0d54794ea64c7
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-19 17:03:09 +0100

    Made some code optimalizations and fixes

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ up_speed = 0
 class InternetSpeedBot:
     def __init__(self, driver_path):
         self.driver = webdriver.Chrome(driver_path)
-        self.down = 0
-        self.up = 0
 
     def get_internet_speed(self):
         global down_speed, up_speed
@@ -31,34 +29,39 @@ class InternetSpeedBot:
         sleep(50)
         down_speed = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
         up_speed = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-        self.driver.quit()
         print(f"down: {down_speed}\nup: {up_speed}")
 
     def tweet_at_provider(self):
         self.driver.get(URL_TWITTER)
-        login = self.driver.find_element(By.ID, )
-        login.click()
-        sleep(2)
-        email_field = self.driver.find_element(By.ID, )
-        email_field.send_keys(EMAIL)
-        pass_field = self.driver.find_element(By.ID, )
-        pass_field.send_keys(PASSWORD)
-        pass_field.send_keys(Keys.ENTER)
-        sleep(2)
-        input_field = self.driver.find_element(By.ID, )
-        input_field.send_keys(f"Hey Internet Provider, why is my internet speed {down_speed}down/{up_speed}up when I pay for 30down/5up?")
 
+        sleep(2)
+        email_field = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input' )
+        pass_field = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+        email_field.send_keys(EMAIL)
+        pass_field.send_keys(PASSWORD)
+        sleep(2)
+        pass_field.send_keys(Keys.ENTER)
+        sleep(4)
+
+        input_field = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey Internet Provider, why is my internet speed {down_speed}down/{up_speed}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        input_field.send_keys(tweet)
+        sleep(3)
+        tweet_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+        sleep(2)
+        self.driver.quit()
 
 
 bot = InternetSpeedBot(DRIVER_PATH)
 bot.get_internet_speed()
-bot.tweet_at_provider()
+if float(down_speed) < (PROMISED_DOWN - 2) or float(up_speed) < (PROMISED_UP - 0.5):
+    bot.tweet_at_provider()
 
 
-#news_tags = soup.findAll("a", href=True)
-#news = [tag.getText() for tag in news_tags]
-
-#article_upvotes = [int(score.getText().split()[0]) for score in soup.findAll("span", class_="score")]
+# news_tags = soup.findAll("a", href=True)
+# news = [tag.getText() for tag in news_tags]
+# article_upvotes = [int(score.getText().split()[0]) for score in soup.findAll("span", class_="score")]
 
 # with open("website.html") as file:
 #     contents = file.read()
